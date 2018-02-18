0ab5fc72d4c62b01c6672408545300a1671a0abf
aduong <aduong@paypal.com>
2018-02-18 00:40:19 -0700

    add template

---
+++ template/assets/js/controller.js
@@ -0,0 +1,9 @@
+var app = angular.module('myApp', ['ngRoute']);
+
+app.controller('myCtrl', function($scope) {
+    $scope.educationBanner = 'http://pbsrg.asu.edu/fmp-online/wp-content/uploads/2014/06/asu-fulton-engineering-school.png';
+    $scope.aboutBanner = '';
+    $scope.projectBanner = ''
+
+    console.log('asdasd')
+})
